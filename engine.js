b2a971fecd3ab8ea60d54a4f65891a0a953dfda6
Phil Douglas <phil.douglas@2and2.com.au>
2019-04-12 11:43:47 +1000

    fix(semver): trying another workflow tag COG-260 #ready-for-qa #time 2m

--- engine.js
+++ engine.js
@@ -27,7 +27,7 @@ module.exports = function (options) {
   });
   var workflowChoices = [
     {name:'In progress', value: '#in-progress'},
-    {name:'Ready for review', value: '#ready'},
+    {name:'Ready for review', value: '#ready-for-qa'},
   ]
 
   return {
